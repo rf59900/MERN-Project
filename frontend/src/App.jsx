a07f7e16252197b5d5e7131dbc319e6b20ae9a09
Ryan Frederick <rrn9475@wmich.edu>
2024-06-07 17:43:23 -0400

    worked on login auth and sign up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import { Board } from "./pages/Board";
 import { ViewPost } from "./pages/ViewPost";
 import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/boards/:board" element={<Board/>} />
       <Route path="/posts/:post" element={<ViewPost/>} />
       <Route path="/login" element={<Login/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
 
     </Routes>
     </>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,9 +2,11 @@ import { useContext, useState } from "react"
 import axiosConfig from "../axiosConfig";
 import AuthContext from "../context/AuthProvider";
 
-export const Login = () => {
+export const SignUp = () => {
     const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+
 
     const [errorMsg, setErrorMsg] = useState('');
 
@@ -13,16 +15,24 @@ export const Login = () => {
     const handleSubmit = async (e) => {
         try {
             e.preventDefault()
+            if (password1 != password2) {
+                setErrorMsg('Passwords do not match')
+                setUsername('')
+                setPassword1('')
+                setPassword2('')
+                return     
+            }
             const response = await axiosConfig.post('/auth', {
                 username: username,
-                password: password
+                password: password1
                 });
                 console.log(response);
             const accessToken = response?.data?.accessToken
             const roles = response?.data?.roles
             setAuth({ username, password, roles, accessToken });
             setUsername('')
-            setPassword('')
+            setPassword1('')
+            setPassword2('')
         } catch (err) {
             if (!err) {
                 setErrorMsg('No response from server.')
@@ -30,14 +40,15 @@ export const Login = () => {
                 setErrorMsg(err?.response?.data?.Error)
             }
             setUsername('')
-            setPassword('')
+            setPassword1('')
+            setPassword2('')
         }
     }
 
     return (
         <>
         <div className="container col-4 mt-5">
-        <h1>Login</h1>
+        <h1>Sign Up</h1>
         <form onSubmit={handleSubmit}>
         { errorMsg ? <div class="alert alert-danger" role="alert">{errorMsg}</div>: null}
             <div class="form-group">
@@ -52,15 +63,30 @@ export const Login = () => {
                     />
             </div>
             <div class="form-group">
-                <label for="password">Password</label>
+                <label for="password1">Password</label>
                 <input 
                     type="password" 
                     class="form-control" 
-                    id="password" 
+                    id="password1" 
                     placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={password1}
+                    onChange={(e) => setPassword1(e.target.value)}
                     />
+            </div>
+            <div class="form-group">
+                <label for="password">Confirm Password</label>
+                <input 
+                    type="password" 
+                    class="form-control" 
+                    id="password2" 
+                    placeholder="Password"
+                    value={password2}
+                    onChange={(e) => setPassword2(e.target.value)}
+                    />
+            </div>
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Upload Avatar Image</label>
+                <input class="form-control" type="file" id="formFile"/>
             </div>
             <br/>
             <button type="submit" class="btn btn-primary">Submit</button>
