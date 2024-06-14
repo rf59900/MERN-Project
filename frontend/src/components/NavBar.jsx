8485adc1acfb100899c325b1f0d6819413932b84
Ryan Frederick <rrn9475@wmich.edu>
2024-06-14 15:54:33 -0400

    did stuff

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,25 +1,35 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthProvider";
 
 export const NavBar = () => {
   const [items, setItems] = useState([1, 2, 3]);
+  const [user, setUser] = useState();
 
   const { auth } = useContext(AuthContext);
   console.log(auth)
+
+  useEffect(() => {
+    setUser(auth?.user);
+  }, [])
   
   return (
     <>
-    <nav className="navbar navbar-expand sticky-top navbar-dark bg-dark">
-      <div className="container text-light">
-      <a className="navbar-brand" href="#">Forum</a>
+    <nav className="navbar navbar-expand sticky-top navbar-dark bg-dark"
+    style={{height: "5rem"}}>
+      <div className="container text-light overflow-hidden">
+      <a className="navbar-brand"><Link to={"/"}>Forum</Link></a>
       <ul className="navbar-nav">
           { Object.keys(auth).length === 0 ? 
           <li className="nav-item">
-          <a href="#" className="nav-link"><Link to={"/login"}>Login</Link></a>
+          <a
+          style={{marginBottom: "0rem"}}className="nav-link"><Link to={"/sign-up"}>Sign Up</Link></a>
+          <a className="nav-link"><Link to={"/login"}>Log in</Link></a>
           </li> :
           <li className="nav-item">
-          <a href="#" className="nav-link"><Link to={"/logout"}>Logout</Link></a>
+          <p
+          style={{marginBottom: "0rem"}}>{auth?.username}</p>
+          <a href="#" className="nav-link"><Link to={"/logout"}>Log out</Link></a>
           </li>
           }
       </ul>
